1b7bd44845ebf5bddad90107a822e3bcd0ed9549
Federico Girolami <68548715+fgirolami29@users.noreply.github.com>
2026-09-19 12:38:03 +0200

    feat(api): expose query-wide search facets

--- includes/Api/SearchResultSet.php
+++ includes/Api/SearchResultSet.php
@@ -5,13 +5,15 @@ namespace CodeCorn\Divi5SearchResults\Api;
 final class SearchResultSet {
     /**
      * @param list<SearchResultItem> $items
+     * @param list<SearchFacet>      $facets
      */
     public function __construct(
         public readonly array $items,
         public readonly int $foundPosts,
         public readonly int $maxPages,
         public readonly int $currentPage,
-        public readonly string $searchTerm
+        public readonly string $searchTerm,
+        public readonly array $facets = array()
     ) {
     }
 }
